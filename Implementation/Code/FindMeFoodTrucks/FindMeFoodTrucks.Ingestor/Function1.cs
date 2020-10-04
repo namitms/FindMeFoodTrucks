3273a90dc9b51b8c268330b587572ad960b93bc7
namitms <namit.t@microsoft.com>
2020-10-04 11:59:07 +1100

    Testing CD for functions
    - Changed function name

--- Implementation/Code/FindMeFoodTrucks/FindMeFoodTrucks.Ingestor/Function1.cs
+++ Implementation/Code/FindMeFoodTrucks/FindMeFoodTrucks.Ingestor/Function1.cs
@@ -5,9 +5,9 @@ using Microsoft.Extensions.Logging;
 
 namespace FindMeFoodTrucks.Ingestor
 {
-    public static class Function1
+    public static class DataIngestor
     {
-        [FunctionName("Function1")]
+        [FunctionName("DataIngestor")]
         public static void Run([TimerTrigger("0 00 0 * * *")]TimerInfo myTimer, ILogger log)
         {
             log.LogInformation($"This is a demo function: {DateTime.Now}");
