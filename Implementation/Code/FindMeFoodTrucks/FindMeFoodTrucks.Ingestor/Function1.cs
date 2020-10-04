d113618a49295480f0cdefc2ad9e817965d15956
namitms <namit.t@microsoft.com>
2020-10-04 12:20:24 +1100

    Code Cleanup
    - Code Cleanup
    - Added GitIgnore file

--- Implementation/Code/FindMeFoodTrucks/FindMeFoodTrucks.Ingestor/Function1.cs
+++ Implementation/Code/FindMeFoodTrucks/FindMeFoodTrucks.Ingestor/Function1.cs
@@ -1,14 +1,13 @@
-using System;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
+using System;
 
 namespace FindMeFoodTrucks.Ingestor
 {
     public static class DataIngestor
     {
         [FunctionName("DataIngestor")]
-        public static void Run([TimerTrigger("0 00 0 * * *")]TimerInfo myTimer, ILogger log)
+        public static void Run([TimerTrigger("0 00 0 * * *")] TimerInfo myTimer, ILogger log)
         {
             log.LogInformation($"This is a demo function: {DateTime.Now}");
         }
