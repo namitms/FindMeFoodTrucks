6ba2df331c8feebbd51a749c020e8c3ae87df5ad
namitms <namit.t@microsoft.com>
2020-10-04 21:13:25 +1100

    Updated CRON expression
    - Updated RON expressions to run every 2 hours

--- Implementation/Code/FindMeFoodTrucks/FindMeFoodTrucks.Ingestor/DataIngestor.cs
+++ Implementation/Code/FindMeFoodTrucks/FindMeFoodTrucks.Ingestor/DataIngestor.cs
@@ -1,47 +1,47 @@
 using FindMeFoodTruck.DAL.Cosmos;
 using FindMeFoodTrucks.Ingestor.Helpers;
-using FindMeFoodTrucks.Models;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.IO;
 
 namespace FindMeFoodTrucks.Ingestor
 {
+    [ExcludeFromCodeCoverage]
     public static class DataIngestor
     {
         [FunctionName("DataIngestor")]
         public static void Run(
-            [TimerTrigger("0 */5 * * * *")] TimerInfo myTimer, ILogger log)
+            [TimerTrigger("0 0 */2 * * *")] TimerInfo myTimer, ILogger log)
         {
             try
             {
                 log.LogInformation($"Starting scheduled task {DateTime.Now}");
+
                 //Set Configuration Object
-                var builder = new ConfigurationBuilder() 
+                var config = new ConfigurationBuilder()
                     .SetBasePath(Directory.GetCurrentDirectory())
                     .AddEnvironmentVariables()
                     .AddJsonFile("./local.settings.json", true)
                     .Build();
 
-                //Extract URL from config
-                string url = builder[ConstantStrings.DATA_API_URL];
-
                 //Get Web response
                 WebRequestHelper wHelper = new WebRequestHelper(log);
-                var jsonResponse = wHelper.GetResponse(url).Result;
 
-                var foodtrucks = JsonConvert.DeserializeObject<List<FoodFacility>>(jsonResponse);
-
-                CosmosDAL cDAL = new CosmosDAL(builder[ConstantStrings.COSMOS_ENDPOINT_URL],
-                    builder[ConstantStrings.COSMOS_PRIMARY_KEY],
-                    builder[ConstantStrings.COSMOS_DATABASE_NAME],
-                    builder[ConstantStrings.COSMOS_CONTAINER_NAME],
+                //DAL
+                CosmosDAL cDAL = new CosmosDAL(config[ConstantStrings.COSMOS_ENDPOINT_URL],
+                    config[ConstantStrings.COSMOS_PRIMARY_KEY],
+                    config[ConstantStrings.COSMOS_DATABASE_NAME],
+                    config[ConstantStrings.COSMOS_CONTAINER_NAME],
                     log);
-                cDAL.WriteData(foodtrucks);
+
+                //Instantiate Orchestrator
+                Orchestrator orc = new Orchestrator();
+
+                //Perform synchronization
+                orc.SynchronizeData(wHelper, cDAL, config, log);
 
                 log.LogInformation($"Completed scheduled task {DateTime.Now}");
             }
