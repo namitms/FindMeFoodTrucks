da7734a4491a1797ab2a62c9c1f18ce3bcbe1937
Namit <68831618+namitms@users.noreply.github.com>
2020-10-05 18:54:23 +1100

    Bug fix (#73)
    * Bug fixes
    
    - Fixed documentations bugs
    - Fixed unit tests
    - Added abstration to Cosmos DAL
    
    * Typo Fix
    
    - Fixed typo on the parameter
    - Added Known issues
    - Added reference to known issues in readme
    
    * Path fix
    
    - Fixing the path for know issues
    
    * Bug Fixes
    
    - Enabled passing headers in swagger
    - Added more known issues
    - Updated Readme

---
+++ Implementation/Code/FindMeFoodTrucks/FindMeFoodTrucks.WebAPI/Filters/Custom HeaderSwaggerAttribute.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace FindMeFoodTrucks.WebAPI.Filters
+{
+    /// <summary>
+    /// Enables Swagger US custom header
+    /// </summary>
+    [ExcludeFromCodeCoverage]
+    public class CustomHeaderSwaggerAttribute : IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            if (string.Equals(context.ApiDescription.HttpMethod, HttpMethod.Get.Method, StringComparison.InvariantCultureIgnoreCase))
+            {
+                operation.Parameters.Add(new OpenApiParameter
+                {
+                    Name = "APIKey",
+                    In = ParameterLocation.Header,
+                    Required = false,
+                    Example = new OpenApiString("MyKey")
+                });
+            }
+        }
+    }
+}
