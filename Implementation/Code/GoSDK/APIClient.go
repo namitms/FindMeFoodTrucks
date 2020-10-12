2308b52f561f1e5280778ff0885e46325b0406aa
namitms <namit.t@microsoft.com>
2020-10-12 13:13:37 +1100

    Go Library partial check in
    - PArtial checkin for Go Lib

--- Implementation/Code/GoSDK/APIClient.go
+++ Implementation/Code/GoSDK/APIClient.go
@@ -2,31 +2,8 @@ package main
 
 import (
     "log"
-    "io/ioutil"
-    "net/http"
 )
 
 func main() {
-    req, err := http.NewRequest(
-        http.MethodGet,
-        "https://findmefoodtrucks-apim.azure-api.net/api/FoodTruck?radius=400&longitude=-122.39772&latitude=37.787539&searchString=fried",
-        nil,
-    )
-    if err != nil {
-        log.Fatalf("error creating HTTP request: %v", err)
-    }
-
-    req.Header.Add("Accept", "application/json")
-    req.Header.Add("Ocp-Apim-Subscription-Key", "ba4a272dd98b4a1da72f6bb12727be97")
-
-    res, err := http.DefaultClient.Do(req)
-    if err != nil {
-        log.Fatalf("error sending HTTP request: %v", err)
-    }
-    responseBytes, err := ioutil.ReadAll(res.Body)
-    if err != nil {
-        log.Fatalf("error reading HTTP response body: %v", err)
-    }
-
-    log.Println("We got the response:", string(responseBytes))
+    log.Println("Hi Go")
 }
